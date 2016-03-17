5883277279e831d3f9000bc95748e5963bc99a17
Karoline Setoue <karoline.kimiko@gmail.com>
2016-03-17 19:14:50 -0300

    node initialization works

--- Winform-node/Form1.Designer.cs
+++ Winform-node/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.btnnode = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnMongo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnnode
@@ -50,12 +53,43 @@
             this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Yay! ";
+            this.label1.Visible = false;
+            // 
+            // btnMongo
+            // 
+            this.btnMongo.Location = new System.Drawing.Point(59, 158);
+            this.btnMongo.Name = "btnMongo";
+            this.btnMongo.Size = new System.Drawing.Size(242, 55);
+            this.btnMongo.TabIndex = 2;
+            this.btnMongo.Text = "Mongo";
+            this.btnMongo.UseVisualStyleBackColor = true;
+            this.btnMongo.Click += new System.EventHandler(this.btnMongo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "uhuul";
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(365, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(959, 327);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnMongo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnnode);
             this.Name = "Form1";
@@ -69,6 +103,9 @@
 
         private System.Windows.Forms.Button btnnode;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMongo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Winform-node/Form1.cs
+++ Winform-node/Form1.cs
@@ -11,8 +11,8 @@ using System.Windows.Forms;
 using MongoDB.Bson;
 using MongoDB.Driver;
 //Additionally, you will frequently add one or more of these using statements:
-//using MongoDB.Driver.Builders;
-//using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
 using MongoDB.Driver.Linq;
 
 namespace Winform_node
@@ -22,10 +22,33 @@ namespace Winform_node
         Process node = new Process();
         Process mongo = new Process();
         string nodeArg = "C:\\node\\mongobase\\index.js";
+        MongoServer _server;
+        MongoDatabase _database;       
 
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void btnMongo_Click(object sender, EventArgs e)
+        {
+
+
+            mongo.StartInfo.UseShellExecute = false;
+            mongo.StartInfo.RedirectStandardOutput = true;
+            mongo.StartInfo.RedirectStandardError = true;
+            mongo.StartInfo.RedirectStandardInput = true;
+            mongo.StartInfo.FileName = "mongod"; //path to node   
+            mongo.StartInfo.Arguments = "";
+            var time = mongo.GetLifetimeService();
+            mongo.Start();
+            label2.Show();
+            textBox1.Text = time.ToString();
+            //string connection = "mongodb://localhost:27017";
+            //var _client = new MongoClient(connection);
+            //_server = _client.GetServer();
+            //_database = _server.GetDatabase("vinifig");
+            // { "The Process object must have the UseShellExecute property set to false in order to redirect IO streams."}
         }
 
         private void btnnode_Click(object sender, EventArgs e)
@@ -34,18 +57,15 @@ namespace Winform_node
             node.StartInfo.RedirectStandardOutput = true;
             node.StartInfo.RedirectStandardError = true;
             node.StartInfo.RedirectStandardInput = true;
-            node.StartInfo.FileName = @"node"; //path to node            
+            node.StartInfo.FileName = "node"; //path to node            
             node.StartInfo.Arguments = @nodeArg;
 
-            mongo.StartInfo.UseShellExecute = true;
-            mongo.StartInfo.RedirectStandardOutput = true;
-            mongo.StartInfo.RedirectStandardError = true;
-            mongo.StartInfo.RedirectStandardInput = true;
-            mongo.StartInfo.FileName = @"mongod"; //path to node            
+                     
 
             node.Start();
-            mongo.Start();
+            label1.Show();
 
         }
+
     }
 }
